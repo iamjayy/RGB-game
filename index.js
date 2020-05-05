ca40d24666d971aa35307a52343f0ac10cd643e5
jason <jason_doan94@yahoo.com>
2020-05-04 21:14:18 -0400

    text fix

--- index.js
+++ index.js
@@ -47,6 +47,9 @@ resetButton.addEventListener("click", function() {
   pickedColor = pickColor();
   //change colorDisplay to match picked color
   colorDisplay.textContent = pickedColor;
+  this.textContent = "New Colors";
+
+  messageDisplay.textContent = "";
   //change colors from squares
   for (var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
